a8f4429855d6e2e5d2e8a1f3652863749416cbbe
Vipul Jain <vipuljain3456@gmail.com>
2022-11-24 14:50:06 +0530

    fetching secret and file details from key vault and storage

--- src/main/java/com/vipul/controller/TextController.java
+++ src/main/java/com/vipul/controller/TextController.java
@@ -1,10 +1,20 @@
 package com.vipul.controller;
 
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.identity.ManagedIdentityCredentialBuilder;
+import com.azure.security.keyvault.secrets.SecretClient;
+import com.azure.security.keyvault.secrets.SecretClientBuilder;
+import com.azure.security.keyvault.secrets.models.KeyVaultSecret;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,6 +23,12 @@ public class TextController {
 
     @Value("${vipul.jain}")
     String age;
+
+    @Value("${logicAppUrl}")
+    String logicAppUrl;
+
+    @Autowired
+    RestTemplate restTemplate;
 
     @GetMapping("/getRandomValue/{index}")
     public String getRandomValue(@PathVariable int index) {
@@ -26,6 +42,37 @@ public class TextController {
     @GetMapping("/")
     public String defaultEntry() {
         return "You are on Welcome Page.";
+    }
+
+    @GetMapping("/get-file-content")
+    public String getFileContent() throws URISyntaxException {
+        //return restTemplate.exchange(logicAppUrl, HttpMethod.GET, null, String.class).getBody();
+        URI uri = new URI(logicAppUrl);
+        return restTemplate.getForEntity(uri, String.class).getBody();
+    }
+
+    @GetMapping("/get-default-secret")
+    public String getDefaultKeyVault() throws URISyntaxException {
+        // get key vault secret from Azure
+        // User have Role to read secret, tested from logic App
+        String keyVaultUri = "https://keyvaultvjj.vault.azure.net/";
+        SecretClient secretClient = new SecretClientBuilder().vaultUrl(keyVaultUri)
+                                                             .credential(new DefaultAzureCredentialBuilder().build())
+                                                             .buildClient();
+        KeyVaultSecret retrievedSecret = secretClient.getSecret("filename");
+        return retrievedSecret.getValue();
+    }
+
+    @GetMapping("/get-managed-secret")
+    public String getManagedKeyVault() throws URISyntaxException {
+        // get key vault secret from Azure
+        // User have Role to read secret, tested from logic App
+        String keyVaultUri = "https://keyvaultvjj.vault.azure.net/";
+        SecretClient secretClient = new SecretClientBuilder().vaultUrl(keyVaultUri)
+                                                             .credential(new ManagedIdentityCredentialBuilder().build())
+                                                             .buildClient();
+        KeyVaultSecret retrievedSecret = secretClient.getSecret("filename");
+        return retrievedSecret.getValue();
     }
 
 }
